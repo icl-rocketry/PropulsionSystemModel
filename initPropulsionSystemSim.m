7b700e311bcc810870263c0a96ac98a4fef7d9ee
storm345 <eddiebbrown05@gmail.com>
2020-10-24 14:43:10 +0100

    Clean up sim inputs, add annotations of equations, etc.. to model

--- initPropulsionSystemSim.m
+++ initPropulsionSystemSim.m
@@ -1,31 +1,54 @@
 clear
-propepProps = load('+HybridMotor/propepinterp.mat');
-nitrousFluidTable = getNitrousFluidTable(); %Nitrous fluid properties for two phase flow model
-PAmbient = 101325; %Pa
-nitrousTankVolume = 0.8*(0.25*pi*(150e-3).^2); %m^3
-injectorSingleHoleA = 0.25*pi*(1.5e-3)^2; %m^2
-feedSystemStartPressure = 64e5;
-nominalFeedSysPipeDiam = 7e-3;
-tankInletA = 0.25*pi*(11e-2)^2;%m^2; Area used after injector for modelled reservoir (Shouldn't matter)
-nominalFeedSysPipeCrossSection = 0.25*pi*(nominalFeedSysPipeDiam)^2;
 
+%%
+%Sim fluid data
+%Combustion products data for thermochem (load once). 
+%In global workspace so can be found by combustion products thermo block. 
+%(Maybe there's a cleaner way to do this with masks??)
+propepProps = load('+HybridMotor/propepinterp.mat');
+sim.nitrousFluidTable = getNitrousFluidTable(); %Nitrous fluid properties for two phase flow model
+sim.PAmbient = 101325; %Pa
+
+%%
+%Feed System properties
+sim.runTank.volume = 0.8*(0.25*pi*(150e-3).^2); %m^3
+sim.pipe.diameter = 7e-3; %m
+sim.pipe.crossSection = 0.25*pi*(sim.pipe.diameter)^2; %m^2
+sim.pipe.startPressure = 64e5; %Pa
+
+sim.preInjectorPipe.diameter = 7e-3; %m
+
+%%
+%Injector properties
+sim.injector.singleHoleA = 0.25*pi*(1.5e-3)^2; %m^2
+sim.injector.postInjectorCrossSection = 0.25*pi*(11e-2)^2;%m^2; Area used after injector for modelled reservoir (Shouldn't matter)
+
+% nitrousTankVolume = 0.8*(0.25*pi*(150e-3).^2); %m^3
+% injectorSingleHoleA = 0.25*pi*(1.5e-3)^2; %m^2
+% feedSystemStartPressure = 64e5;
+% nominalFeedSysPipeDiam = 7e-3;
+% tankInletA = 0.25*pi*(11e-2)^2;%m^2; Area used after injector for modelled reservoir (Shouldn't matter)
+% nominalFeedSysPipeCrossSection = 0.25*pi*(nominalFeedSysPipeDiam)^2;
+
+%%
 %Combustion chamber properties
-combutionChamber.OFRatioInitial = 6.5;
-combutionChamber.mdotOxInitial = 0.3; %kg/s
-combutionChamber.rhoFuel = 953; %kg/m^3
-combutionChamber.combustionEfficiency = 0.95;
-combutionChamber.PChamberInit = 30e5;
-combutionChamber.portLength = 0.5967; %m. "Lp" in SPAD
+sim.combustionChamber.OFRatioInitial = 6.5;
+sim.combustionChamber.mdotOxInitial = 0.3; %kg/s
+sim.combustionChamber.rhoFuel = 953; %kg/m^3 (density)
+sim.combustionChamber.combustionEfficiency = 0.95;
+sim.combustionChamber.PChamberInit = 30e5;
+sim.combustionChamber.portLength = 0.5967; %m. "Lp" in SPAD
 %Regression rate parameters, empirical
 %using numbers from adam bakers excel file.
-combutionChamber.regRateParams.a = 2.3600e-5;
-combutionChamber.regRateParams.n = 0.6050;
-combutionChamber.regRateParams.m = 0;
+sim.combustionChamber.regRateParams.a = 2.3600e-5;
+sim.combustionChamber.regRateParams.n = 0.6050;
+sim.combustionChamber.regRateParams.m = 0;
 %Port configuration, circular
-combutionChamber.initialPortDiameter = 0.0403; %m^2
-combutionChamber.initialFuelWebThickness = 0.0098;
+sim.combustionChamber.initialPortDiameter = 0.0403; %m (Grain inner diam)
+sim.combustionChamber.initialFuelWebThickness = 0.0098; %m (Grain outer diam - inner diam)
 
+%%
 %Nozzle properties
-nozzle.throatArea = 2.4575e-4; %m^2
-nozzle.expansionRatio = 2.3325; %m^2
-nozzle.thrustEfficiencyFactor = 0.85; %lambda
+sim.nozzle.throatArea = 2.4575e-4; %m^2
+sim.nozzle.expansionRatio = 2.3325; %m^2
+sim.nozzle.thrustEfficiencyFactor = 0.85; %lambda
