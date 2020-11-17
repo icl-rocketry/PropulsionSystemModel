affb2b9e220e38c539d90226707ce41bbeeed0cf
luc <luc@DESKTOP-OBU963D>
2020-11-17 19:10:30 +0000

    Not working, added heating element + starting to add controls as matlab function

--- initFillingSystemSim.m
+++ initFillingSystemSim.m
@@ -1,2 +1,7 @@
 
 sim.extTank.volume = 9; %volume of external tank
+sim.Heater.SA = 1; %SA of heater surrounding tank
+sim.Heater.temp = 293; %initialises heater at room temperature
+sim.Heater.weight = 0.1; %weight of heater. Very unsure as to the effect of this
+
+sim.fillValve.aperture = 0.005; %maximum open-ness of valve
