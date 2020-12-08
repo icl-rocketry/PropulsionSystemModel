b9e3cb7c11d5e8b2f09b306ced4fd6fac703ad3d
storm345 <eddiebbrown05@gmail.com>
2020-12-08 14:41:34 +0000

    Pablo model

--- initPropulsionSystemSim.m
+++ initPropulsionSystemSim.m
@@ -1,3 +1,5 @@
+%VALUES ARE FOR PABLO
+
 clear
 applyReceiverAccumulatorPatch();
 %initFillingSystemSim()
@@ -12,19 +14,18 @@ Sim.PAmbient = 101325; %Pa
 
 %%
 %Feed System properties
-Sim.runTank.volume = 0.8*(0.25*pi*(150e-3).^2); %m^3
-Sim.runTank.PInit = 64e5; %Pascal. Very temperature dependent. Eg. 0C->31bar, 20C->50bar, 30C->63bar
+Sim.runTank.volume = 18; %m^3 BOC J sized nitrous oxide cylinder
+Sim.runTank.PInit = 31e5; %Pascal. Very temperature dependent (=Vapour pressure @ temp). Eg. 0C->31bar, 20C->50bar, 30C->63bar
 Sim.pipe.diameter = 7e-3; %m
 Sim.pipe.crossSection = 0.25*pi*(Sim.pipe.diameter)^2; %m^2
 Sim.pipe.startPressure = 64e5; %Pa (40e5 or so for Pablo)
-Sim.pipe.mdotOxInitial = 0.3; %kg/sec (0.05 or so for Pablo)
+Sim.pipe.mdotOxInitial = 0.05; %kg/sec (0.05 or so for Pablo)
 
 Sim.preInjectorPipe.diameter = 7e-3; %m
 Sim.preInjectorPipe.crossSection = 0.25*pi*Sim.preInjectorPipe.diameter^2; %m^2
 
 %%
 %Injector properties
-Sim.injector.singleHoleA = 0.25*pi*(1.5e-3)^2; %m^2
 Sim.injector.postInjectorCrossSection = 0.25*pi*(11e-2)^2;%m^2; Area used after injector for modelled reservoir (Shouldn't matter)
 
 %%
@@ -33,7 +34,7 @@ Sim.combustionChamber.OFRatioInitial = 6.5;
 Sim.combustionChamber.mdotOxInitial = Sim.pipe.mdotOxInitial; %kg/s
 Sim.combustionChamber.rhoFuel = 953; %kg/m^3 (density)
 Sim.combustionChamber.combustionEfficiency = 0.95;
-Sim.combustionChamber.PChamberInit = 30e5;
+Sim.combustionChamber.PChamberInit = 20e5;
 Sim.combustionChamber.portLength = 0.5967; %m. "Lp" in SPAD
 %Regression rate parameters, empirical
 %using numbers from adam bakers excel file.
@@ -46,6 +47,6 @@ Sim.combustionChamber.initialFuelWebThickness = 0.0098; %m (Grain outer diam - i
 
 %%
 %Nozzle properties
-Sim.nozzle.throatArea = 2.4575e-4; %m^2 (4.07e-5 for Pablo)
-Sim.nozzle.expansionRatio = 2.3325; %m^2 (2.6 for Pablo)
+Sim.nozzle.throatArea = 4.07e-5; %m^2
+Sim.nozzle.expansionRatio = 2.6; %m^2
 Sim.nozzle.thrustEfficiencyFactor = 0.85; %lambda
