ec37b8d42b291b977d805ea5cbe020e97a7014df
storm345 <eddiebbrown05@gmail.com>
2021-02-02 17:09:44 +0000

    Update sim for hypnos, decrease phase change time constant to 0.01 to prevent oscillation (Need some checking what a reasonable value for this is)

--- initPropulsionSystemSim.m
+++ initPropulsionSystemSim.m
@@ -13,12 +13,14 @@ Sim.PAmbient = 101325; %Pa
 
 %%
 %Feed System properties
-Sim.runTank.volume = 0.8*(0.25*pi*(150e-3).^2); %m^3
+Sim.phaseChangeTimeConstant = 0.01; %sec. Arbitrary value, please refine
+
+Sim.runTank.volume = 0.013784;% m^3 (Source: Phil). 0.8*(0.25*pi*(150e-3).^2); %m^3
 Sim.runTank.PInit = 60e5; %Pascal. Very temperature dependent. Eg. 0C->31bar, 20C->50bar, 30C->63bar
 Sim.pipe.diameter = 7e-3; %m
 Sim.pipe.crossSection = 0.25*pi*(Sim.pipe.diameter)^2; %m^2
 Sim.pipe.startPressure = Sim.runTank.PInit; %Pa (40e5 or so for Pablo)
-Sim.pipe.mdotOxInitial = 0.1; %kg/sec (0.05 or so for Pablo)
+Sim.pipe.mdotOxInitial = 0.25; %kg/sec (0.05 or so for Pablo)
 
 Sim.preInjectorPipe.diameter = 7e-3; %m
 Sim.preInjectorPipe.crossSection = 0.25*pi*Sim.preInjectorPipe.diameter^2; %m^2
@@ -35,7 +37,7 @@ Sim.combustionChamber.mdotOxInitial = Sim.pipe.mdotOxInitial; %kg/s
 Sim.combustionChamber.rhoFuel = 953; %kg/m^3 (density)
 Sim.combustionChamber.combustionEfficiency = 0.95;
 Sim.combustionChamber.PChamberInit = 27e5;
-Sim.combustionChamber.portLength = 0.26; %m. "Lp" in SPAD
+Sim.combustionChamber.portLength = 600e-3; %m. "Lp" in SPAD
 %Regression rate parameters, empirical
 Sim.combustionChamber.useOxFluxRegRateEquation = 1; %If 1 then uses rdot=a*Gox^n, if 0 then uses rdot=a*Gprop^n*length^m
 %McCormick et all 2005 for FR5560
@@ -50,12 +52,12 @@ Sim.combustionChamber.regRateParams.m = NaN; %Unused
 % Sim.combustionChamber.regRateParams.m = 0;
 %Port configuration, circular
 %Values for potentially possible pablo
-Sim.combustionChamber.initialPortDiameter = 23e-3; %m (Grain inner diam)
-grainOuterDiam = 50e-3; %m
+Sim.combustionChamber.initialPortDiameter = 26e-3; %m (Grain inner diam)
+grainOuterDiam = 63.5e-3; %m
 Sim.combustionChamber.initialFuelWebThickness = grainOuterDiam-Sim.combustionChamber.initialPortDiameter; %m (Grain outer diam - inner diam)
 
 %%
 %Nozzle properties
 Sim.nozzle.throatArea = 2.4575e-4; %m^2 (4.07e-5 for Pablo)
 Sim.nozzle.expansionRatio = 3.9; %m^2 (2.6 for Pablo)
-Sim.nozzle.thrustEfficiencyFactor = 0.85; %lambda
+Sim.nozzle.thrustEfficiencyFactor = 0.9; %lambda
