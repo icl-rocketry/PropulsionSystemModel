712e20971228b06d8906434fa32ed1e3b4dbd451
storm345 <eddiebbrown05@gmail.com>
2021-03-15 15:15:01 +0000

    Decrease initial condition for chamber pressure again. Allows simulation to initialize correctly for lower throttle settings

--- initPropulsionSystemSim.m
+++ initPropulsionSystemSim.m
@@ -43,7 +43,7 @@ Sim.combustionChamber.OFRatioInitial = 6.5;
 Sim.combustionChamber.mdotOxInitial = Sim.pipe.mdotOxInitial; %kg/s
 Sim.combustionChamber.rhoFuel = 993.825; %kg/m^3 (density)
 Sim.combustionChamber.combustionEfficiency = 0.95;
-Sim.combustionChamber.PChamberInit = 31e5;
+Sim.combustionChamber.PChamberInit = 15e5;
 %Articially prevent chamber pressure exceeding this, can fix non-physical
 %spikes at very start of simulation
 Sim.combustionChamber.artificalPChamberMax = 40e5; %Up to 60e5 is max we have data for;
@@ -76,5 +76,5 @@ Sim.combustionChamber.initialFuelWebThickness = grainOuterDiam-Sim.combustionCha
 %%
 %Nozzle properties
 Sim.nozzle.throatArea = 2.4575e-4; %m^2 (4.07e-5 for Pablo)
-Sim.nozzle.expansionRatio = 5.2592; % (2.6 for Pablo) <-- design point gamma = 1.1484 at P = 30bar and OF = 8
+Sim.nozzle.expansionRatio = 5.15;%2592; % (2.6 for Pablo) <-- design point gamma = 1.1484 at P = 30bar and OF = 8
 Sim.nozzle.thrustEfficiencyFactor = 0.9; %lambda
